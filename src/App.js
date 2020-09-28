d61163d22d6e490108005067fe0d69d6e1e4655d
Andrew Joy <56839275+AndrewJoyT@users.noreply.github.com>
2020-09-28 10:29:04 -0500

    cermatitest

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import Notification from "./component/notification";
+import Header from "./component/header";
+import Highlight from "./component/highlight";
+import Footer from "./component/footer";
+import Panel from "./component/panel";
+
+function App({ notif }) {
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <Notification />
+      <div
+        className="atas"
+        style={{
+          paddingTop: notif ? null : 0,
+          transition: "0.2s",
+          maxWidth: "1366px",
+        }}
+      >
+        <Header />
+        <Highlight />
+        <Footer />
+        <Panel />
+      </div>
+    </div>
+  );
+}
+
+const stp = ({ reducer }) => {
+  return {
+    notif: reducer.notif,
+  };
+};
+
+export default connect(stp)(App);
